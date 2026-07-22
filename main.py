dd2cac2b1c37334c6e5f252353b5f59da8a1466b
breadpakoda <adityasing272@gmail.com>
2026-07-22 23:10:49 +0530

    manual execution of the loop done

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import (
     SystemMessage,
     HumanMessage,
-    AIMessage
+    AIMessage,
+    ToolMessage
 )
 from tools.db_tool import query_executor
 from tools.input_tool import take_user_input
@@ -22,7 +23,8 @@ context=[
 You are a My sql ReAct agent , your task is to generate the mysql queries accrding to the users nees
 """)
 ]
-
+tools={"query_executor":query_executor,
+       "take_user_input" :take_user_input}
 tool_binded_llm=llm.bind_tools(
     [
         query_executor,
@@ -33,12 +35,20 @@ while True:
     user_input=input("Query: ")
     context.append(HumanMessage(content=user_input))
 
-    response=tool_binded_llm.invoke(context)
-    context.append(AIMessage(response.content))
-    
-    if not response.content:
-        
-        print(response.tool_calls)
+   
 
-    else:
-        print(response.content)
+
+
+    while True:
+        response=tool_binded_llm.invoke(context)
+        context.append(response)
+    
+        if response.tool_calls:
+            
+            context.append(ToolMessage(content=tools[response.tool_calls[0]['name']].invoke(response.tool_calls[0]['args']),
+                                       tool_call_id=response.tool_calls[0]['id'])) 
+
+        else:
+            print(response.content)
+            break
+# [{'name': 'query_executor', 'args': {'query': 'SHOW DATABASES;'}, 'id': 'call-f9cff72f-50c4-4934-b815-4e7c05e42af6', 'type': 'tool_call'}]
